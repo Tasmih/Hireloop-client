e259b768f55d90d0f819c32291143af1e9c45b17
Tasmih Rob Mow <tasmih.mow20@gmail.com>
2026-06-15 03:16:15 +0600

    : implement make recruiter/seeker actions with confirmation

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,30 +1,33 @@
+import { redirect } from "next/navigation";
 import { getUserToken } from "./session";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const authHeader = async()=>{
-    const token= await getUserToken();
-    const header=token ? {
-        authorization:`Bearer ${token}`
-    }: {}
-    return header ;
-
+export const authHeader = async () => {
+    const token = await getUserToken();
+    const header = token ? {
+        authorization: `Bearer ${token}`
+    } : {};
+    return header;
 }
 
 export const serverFetch = async (path) => {
     const res = await fetch(`${baseUrl}${path}`);
-    // handle 401, 404, 403
-    return res.json();
+    
+    return handleStatusCode(res);
 }
 
+export const protectedFetch = async (path) => {
+    const res = await fetch(`${baseUrl}${path}`,
+        {
+            headers: await authHeader()
+        }
+    );
 
-export const protectedFetch = async(path)=>{
-const res = await fetch(`${baseUrl}${path}`, {
-headers: await authHeader()
-});
-// handle 401, 404, 403
-return res.json();
-};
+    // handle 401, 403
+
+    return handleStatusCode(res);
+}
 
 
 export const serverMutation = async (path, data, method = 'POST') => {
@@ -37,7 +40,20 @@ export const serverMutation = async (path, data, method = 'POST') => {
         body: JSON.stringify(data),
     });
 
-    // handle 401, 404, 403
-console.log('status code', res.status)
-    return res.json();
+
+    return handleStatusCode(res);
 }
+
+
+// handle 401, 404, 403
+const handleStatusCode = res => {
+    if (res.status === 401) {
+        redirect('/unauthorized')
+    }
+    else if (res.status === 403) {
+        redirect('/forbidden');
+    }
+
+    return res.json()
+}
+
