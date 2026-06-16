274020387383b8d132c8e8004555101a58ea7a83
Tasmih Rob Mow <tasmih.mow20@gmail.com>
2026-06-17 02:43:23 +0600

    remove run() error and fix MongoDB connection flow

--- src/app/dashboard/recruiter/company/page.jsx
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -3,22 +3,7 @@ import CompanyProfile from './CompanyProfile';
 import { getUserSession } from '@/lib/core/session';
 import { getRecruiterCompany } from '@/lib/api/companies';
 
-const CompanyPage = async () => {
 
-    const user = await getUserSession();
-    const company = await getRecruiterCompany(user?.id);
-    console.log('company before create', company)
-    return (
-        <div>
-            <CompanyProfile recruiter={user} recruiterCompany={company}></CompanyProfile>
-        </div>
-    );
-};
-
-export default CompanyPage;import React from 'react';
-import CompanyProfile from './CompanyProfile';
-import { getUserSession } from '@/lib/core/session';
-import { getRecruiterCompany } from '@/lib/api/companies';
 
 const CompanyPage = async () => {
 
